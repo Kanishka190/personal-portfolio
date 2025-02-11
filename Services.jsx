54b9368d161d8e9839e391eb9bc6932d7f8f3ca0
Kanishka190 <112696404+Kanishka190@users.noreply.github.com>
2025-02-11 12:24:49 +0530

    Add files via upload

---
+++ Services.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import { FaAppStore } from 'react-icons/fa'
+import { motion } from "motion/react"
+import { useInView } from 'react-intersection-observer'
+
+const Services = () => {
+
+    const servicesData = [
+        {
+            title: "App Design",
+            description:
+                "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis quidem ipsam sint ut nam illo commodi error dolorem! Vitae qui odit laboriosam dignissimos dolorum? Exercitationem sint eius assumenda accusamus omnis! "
+        },
+        {
+            title: "App Design",
+            description:
+                "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis quidem ipsam sint ut nam illo commodi error dolorem! Vitae qui odit laboriosam dignissimos dolorum? Exercitationem sint eius assumenda accusamus omnis! "
+        },
+        {
+            title: "App Design",
+            description:
+                "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis quidem ipsam sint ut nam illo commodi error dolorem! Vitae qui odit laboriosam dignissimos dolorum? Exercitationem sint eius assumenda accusamus omnis! "
+        },
+    ];
+
+    const { ref, inView } = useInView({
+        triggerOnce: true,
+        threshold: 0.2,
+    });
+    return (
+        <div id ="services" className='text-white py-16'>
+            <motion.div
+                ref={ref}
+                initial={{ opacity: 0, y: 50 }}
+                animate={inView ? { opacity: 1, y: 0 } : {}}
+                transition={{ duration: 0.5 }}
+                className='container mx-auto px-4 text-center'>
+                <h2 className='text-3xl md:text-4xl font-bold underline mb-8'>Services</h2>
+                <p className='mb-12 text-gray-400 '>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis quidem ipsam sint ut nam illo commodi error dolorem! Vitae qui odit laboriosam dignissimos dolorum? Exercitationem sint eius assumenda accusamus omnis!
+                </p>
+
+                <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8'>
+                    {servicesData.map((Service, index) => (
+                        <motion.div
+                            ref={ref}
+                            initial={{ opacity: 0, scale:0.8 }}
+                            animate={inView ? { opacity: 1, scale: 1} : {opacity: 0, scale:0.8 }}
+                            transition={{ duration: 0.5, delay: index * 0.2}}
+                            key={index}
+                            className='bg-[#1c1a2b] rounded-lg p-6 text-center hover: shadow-lg hover:shadow-purple-500 transition-shadow duration-300 '
+                        >
+                            <FaAppStore className='text-purple-500 text-4xl sm:text-5xl lg:text-6xl mb-4 mx-auto' />
+                            <h3 className='text-lg sm:text-3xl lg:text-2xl font-semibold mb-2'>{Service.title}</h3>
+                            <p className='text-sm,:text-base lg:text-lg text-gray-400'>{Service.description}</p>
+                        </motion.div>
+                    ))}
+                </div>
+            </motion.div>
+        </div>
+    )
+}
+
+export default Services
